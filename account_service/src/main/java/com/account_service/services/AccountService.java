c45fdf6fc09337625ade84f33d30740e9c0cbcc5
NOVIUS-SHRIKANTSHINDE <scshinde9096@gmail.com>
2024-03-26 22:00:03 +0530

    new

---
+++ account_service/src/main/java/com/account_service/services/AccountService.java
@@ -0,0 +1,37 @@
+package com.account_service.services;
+
+import com.account_service.entity.Account;
+
+import java.util.List;
+
+public interface AccountService {
+
+    // Create
+    Account create(Account account);
+
+    // Get accounts
+    List<Account> getAccounts();
+
+    // Get single account
+    Account getAccount(String id);
+
+    // Get single account using customerId
+    List<Account> getAccountByCustomerId(String customerId);
+
+    // Update Account
+    Account updateAccount(String id, Account account);
+
+    // Update Balance
+    Account addBalance(String id, int amount, String customerId);
+    Account withdrawBalance(String id, int amount, String customerId);
+
+    // Delete
+    void delete(String id);
+
+    void deleteAccountUsingCustomerId(String customerId);
+
+	Account login(String accountId, String transactionPin);
+
+
+	
+}
